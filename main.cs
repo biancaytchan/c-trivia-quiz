531dc677a552661f791bea19dbcc4ee6af9414f5
Bianca Chan <68483860+biancaytchan@users.noreply.github.com>
2022-10-15 18:41:34 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    
+    Console.WriteLine("Trivia Quiz: Guess Where They're From")
+    
+    string[] questions = {"Which anime/manga is 'Nanami Kento' from?", 
+                          "Which anime/manga is 'Kunikida Doppo' from?", 
+                          "Which anime/manga is 'Aki Hayakawa' from?", 
+                          "Which anime/manga is 'Akihiko Kaji' from?", 
+                          "Which anime/manga is 'Asakaze' from?",
+                          "Which anime/manga is 'Suzuya Juuzou' from?"};
+
+    string[] answers = {"Sonny Boy",
+                        "Jujutsu Kaisen",
+                        "Bungo Stray Dogs",
+                        "Given",
+                        "Chainsaw Man",
+                        "Tokyo Ghoul",};
+
+
+
+    
+  }
+}
